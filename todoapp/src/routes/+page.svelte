b80315a8379c729c0d6c12ec59682ee753594792
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2023-07-05 21:28:46 +0000

    add remove and persistant updates added to taskapp

--- todoapp/src/routes/+page.svelte
+++ todoapp/src/routes/+page.svelte
@@ -1,17 +1,56 @@
 <script>
     import axios from 'axios';
     import { onMount } from 'svelte';
+    
     let tasks = [];
-    let newTask = '';
+    let newTask = {
+        text: "",
+        completed: false
+    };
 
     async function getTasks() {
         try {
-        const response = await axios.get('http://localhost:8000/tasks/');
-        tasks = response.data;
+            const response = await axios.get('http://localhost:8000/tasks/');
+            tasks = response.data;
         } catch (error) {
         console.error(error);
         }
     }
+
+    async function updateTask(task) {
+        try {
+            await axios.put(`http://localhost:8000/tasks/${task.id}/`, task);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function deleteTask(task) {
+        try {
+            await axios.delete(`http://localhost:8000/tasks/${task.id}/`, task);
+            tasks = tasks.filter(item => item.id !== task.id);
+        } catch (error) {
+            console.error(error);
+        }
+    }
+
+    async function addTask() {
+        if (newTask.text.trim() === "") {
+            return
+        }
+        try {
+            const res = await axios.post('http://localhost:8000/tasks/', newTask);
+            tasks = [...tasks, res.data];
+            newTask = {
+                text: "",
+                completed: false
+            };
+        }  catch (error) {
+            console.error(error);
+        }
+        
+    }
+
     onMount(getTasks);
 </script>
 <main>
@@ -20,15 +59,15 @@
         <ul>
             {#each tasks as task (task.id) }
                 <li>
-                    <input type="text" bind:value={task.text}>
-                    <input type="checkbox" bind:checked={task.completed}>
-                    <button>x</button>
+                    <input type="text" bind:value={task.text} on:change={() => updateTask(task)}>
+                    <input type="checkbox" bind:checked={task.completed} on:change={() => updateTask(task)}>
+                    <button on:onclick={() => deleteTask(task)}>x</button>
                 </li>
             {/each}
             <li>
-                <input type="text">
-                <input type="checkbox">
-                <button>+</button>
+                <input type="text" bind:value={newTask.text}>
+                <input type="checkbox" bind:value={newTask.completed}>
+                <button on:click={()=> addTask()}>+</button>
             </li>
         </ul>
     {:else}
