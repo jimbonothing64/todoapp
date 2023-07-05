0898722d62412ee3586e8ee9be07ce5c993ec6b6
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2023-07-05 20:48:52 +0000

    Create skeleton of todo page

---
+++ todoapp/src/routes/+page.svelte
@@ -0,0 +1,19 @@
+<script>
+    let tasks = ['Hi'];
+    let newTask = '';
+</script>
+<main>
+    <h1>Todos:</h1>
+    {#if tasks.length > 0}
+        <ul>
+            <li>
+                <input type="text">
+                <input type="checkbox">
+                <button>x</button>
+            </li>
+        </ul>
+    {:else}
+        <p>You have nothing to do :)</p>
+    {/if}
+</main>
+<style></style>
