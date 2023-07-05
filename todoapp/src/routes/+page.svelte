0a5d59922a62121ee259080faf4d615dcb2f6c56
jimbonothing64 <61300251+jimbonothing64@users.noreply.github.com>
2023-07-05 20:57:32 +0000

    add list feature to taskapp

--- todoapp/src/routes/+page.svelte
+++ todoapp/src/routes/+page.svelte
@@ -1,19 +1,39 @@
 <script>
-    let tasks = ['Hi'];
+    import axios from 'axios';
+    import { onMount } from 'svelte';
+    let tasks = [];
     let newTask = '';
+
+    async function getTasks() {
+        try {
+        const response = await axios.get('http://localhost:8000/tasks/');
+        tasks = response.data;
+        } catch (error) {
+        console.error(error);
+        }
+    }
+    onMount(getTasks);
 </script>
 <main>
     <h1>Todos:</h1>
     {#if tasks.length > 0}
         <ul>
+            {#each tasks as task (task.id) }
+                <li>
+                    <input type="text" bind:value={task.text}>
+                    <input type="checkbox" bind:checked={task.completed}>
+                    <button>x</button>
+                </li>
+            {/each}
             <li>
                 <input type="text">
                 <input type="checkbox">
-                <button>x</button>
+                <button>+</button>
             </li>
         </ul>
     {:else}
         <p>You have nothing to do :)</p>
     {/if}
+
 </main>
 <style></style>
